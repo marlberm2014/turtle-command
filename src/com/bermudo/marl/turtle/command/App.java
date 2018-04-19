4596665fbf2317bdad0113a69b890b422f1d604d
Bermudo Marl <marl.bermudo@nokia.com>
2018-04-19 19:58:33 +0800

    New Feature: Initialize maven-java project Reviewer: N/A Description: Initialize maven-java project in eclipse
    Change-Id: Ie017325195b37094bf8a756924cfc1251155edcd

---
+++ src/com/bermudo/marl/turtle/command/App.java
@@ -0,0 +1,13 @@
+package com.bermudo.marl.turtle.command;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
